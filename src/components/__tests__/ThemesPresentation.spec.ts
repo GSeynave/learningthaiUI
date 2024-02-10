025c6e8631a594ee833a69d2e93ad892eae84098
GSeynave <gauthier.seynave@gmail.com>
2024-02-10 17:22:35 +0100

    fix test

--- src/components/__tests__/ThemesPresentation.spec.ts
+++ src/components/__tests__/ThemesPresentation.spec.ts
@@ -1,21 +1,19 @@
 import { describe, it, expect } from 'vitest'
 import { shallowMount, mount } from '@vue/test-utils'
-
-import { mount } from '@vue/test-utils'
 import ThemesPresentation from '../ThemesPresentation.vue'
 import ThemeItem from '../ThemeItem.vue'
 
 describe('ThemesPresentation', () => {
   it('renders properly', () => {
     const wrapper = mount(ThemesPresentation, {
-      globals:{
+      globals: {
         components: { ThemeItem }
       }
-    });
-    expect(wrapper).toBeDefined();
+    })
+    expect(wrapper).toBeDefined()
   }),
-  it('init x themeItem', () => {
-    const wrapper = shallowMount(ThemesPresentation);
-    expect(wrapper.html()).contains("theme-item");
-  })
+    it('init x themeItem', () => {
+      const wrapper = shallowMount(ThemesPresentation)
+      expect(wrapper.html()).contains('theme-item')
+    })
 })
